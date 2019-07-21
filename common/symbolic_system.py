79feb168f502c9fb233bdc9317ad897c722e618f
albertwu <wualbert@mit.edu>
2019-07-20 20:26:17 -0700

    untested multimodal system

--- common/symbolic_system.py
+++ common/symbolic_system.py
@@ -119,7 +119,6 @@ class DTContinuousSystem:
     def get_current_state(self):
         return extract_variable_value_from_env(self.dynamics.x, self.env)
 
-
 class DTHybridSystem:
     def __init__(self, f_list, x, u, c_list, initial_env=None, input_limits=None):
         '''
@@ -127,13 +126,14 @@ class DTHybridSystem:
         :param f_list: numpy array of system dynamics modes
         :param x:
         :param u:
-        :param c_list: numpy array of functions C_i(x,u) describing when the system belong in that mode.
-                        C_i(x,u) >= 0 when system is in mode i.
+        :param c_list: numpy array of Pydrake symbolic expressions c_i(x,u) describing when the system belong in that mode.
+                        c_i(x,u) is a vector of functions describing the mode. All of c_i(x,u) >= 0 when system is in mode i.
                         Modes should be complete and mutually exclusive.
         :param initial_env: A dictionary "environment" specifying the initial state of the system
         :param input_limits: Input limits of the system
         '''
         assert f_list.shape[0] == c_list.shape[0]
+        self.mode_count = f_list.shape[0]
         self.dynamics_list = np.asarray([ContinuousDynamics(f,x,u) for f in f_list])
         if initial_env is None:
             self.env = {}
@@ -146,6 +146,9 @@ class DTHybridSystem:
         else:
             self.input_limits = input_limits
         self.c_list = c_list
+        # Check the mode the system is in
+        self.current_mode = -1
+        #TODO
 
     def foward_step(self, u=None, linearlize=False, modify_system=True, step_size = 1e-3, return_as_env = False,
                     return_mode = False):
@@ -163,8 +166,7 @@ class DTHybridSystem:
         delta_x = None
         mode = -1
         for i, c_i in enumerate(self.c_list):
-            #FIXME:
-            if c_i(x, u) >= 0:
+            if sym.Evaluate(c_i(new_env)).all() >= 0:
                 delta_x = self.dynamics_list[i].evaluate_xdot(new_env, linearlize)*step_size
                 mode = i
                 break
@@ -183,28 +185,25 @@ class DTHybridSystem:
         else:
             return extract_variable_value_from_env(self.dynamics_list[mode].x, new_env), mode
 
-    def get_reachable_zonotope(self, state, step_size=1e-2, return_mode =False):
-        # Check for which mode the system is in
+    def get_reachable_zonotopes(self, state, step_size=1e-2):
+        zonotopes_list = []
         for mode, c_i in enumerate(self.c_list):
-            #FIXME: c_i usage?
-            if c_i(state, u) >= 0:
-                current_linsys = self.get_linearization(mode, state)
-                u_bar = (self.input_limits[1, :] + self.input_limits[0, :]) / 2
-                u_diff = (self.input_limits[1, :] - self.input_limits[0, :]) / 2
-                # print(current_linsys.A, current_linsys.B, current_linsys.c)
-                x = np.ndarray.flatten(
-                    np.dot(current_linsys.A * step_size + np.eye(current_linsys.A.shape[0]), state)) + \
-                    np.dot(current_linsys.B * step_size, u_bar) + np.ndarray.flatten(current_linsys.c * step_size)
-                x = np.atleast_2d(x).reshape(-1, 1)
-                assert (len(x) == len(state))
-                G = np.atleast_2d(np.dot(current_linsys.B * step_size, np.diag(u_diff)))
-                # print('x', x)
-                # print('G', G)
-                if return_mode:
-                    return zonotope(x, G), mode
-                else:
-                    return zonotope(x, G)
+            # FIXME: check if the mode is reachable
+            current_linsys = self.get_linearization(mode, state)
+            u_bar = (self.input_limits[1, :] + self.input_limits[0, :]) / 2
+            u_diff = (self.input_limits[1, :] - self.input_limits[0, :]) / 2
+            # print(current_linsys.A, current_linsys.B, current_linsys.c)
+            x = np.ndarray.flatten(
+                np.dot(current_linsys.A * step_size + np.eye(current_linsys.A.shape[0]), state)) + \
+                np.dot(current_linsys.B * step_size, u_bar) + np.ndarray.flatten(current_linsys.c * step_size)
+            x = np.atleast_2d(x).reshape(-1, 1)
+            assert (len(x) == len(state))
+            G = np.atleast_2d(np.dot(current_linsys.B * step_size, np.diag(u_diff)))
+            # print('x', x)
+            # print('G', G)
+            zonotopes_list.append(zonotope(x,G))
 
+        return np.asarray(zonotopes_list)
 
     def get_linearization(self, mode, state=None):
         if state is None:
@@ -223,4 +222,4 @@ class DTHybridSystem:
         return env
 
     def get_current_state(self):
-        return extract_variable_value_from_env(self.dynamics[self.current_mode].x, self.env)
+        return extract_variable_value_from_env(self.dynamics_list[self.current_mode].x, self.env)
